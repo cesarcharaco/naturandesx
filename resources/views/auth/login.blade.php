19f8184280d925381a2bf701ec3fe7f9095f7664
Jhon-Dante <imagine.breacker@gmail.com>
2020-09-17 14:08:51 -0400

    Nuevo inicio de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,68 @@
 @extends('layouts.app_login')
-
+<style type="text/css">
+  body{
+    background-image: url("{{ asset('img/fondoN.jpg') }}") !important;
+    background-position: center !important;
+    background-repeat: no-repeat !important;
+    background-size: cover !important;
+  }
+  h2{
+    color: white !important;
+  }
+  .card-black{
+    /*background-color: black !important;*/
+    border-radius: 0px !important;
+    padding: 20px;
+    background: rgba(0, 0, 0, 0.6) !important;
+    color: black;
+    font: 18px Arial, sans-serif;
+  }
+  .input{
+    color:orange !important;
+    border-radius: 10px !important;
+    text-align: center;
+  }
+</style>
 @section('content')
-<div class="login-box">
-  <div class="login-logo">
-    <a href="{{ route('login') }}"><b>Natur</b>Andes</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Inciar sesión</p>
 
-      <form action="{{ route('login') }}" method="post">
-        @csrf
-        <div class="input-group mb-3">
-          <input id="login" type="text" class="form-control @if ($errors->has('usuario') || $errors->has('email')) is-invalid @endif" name="login" value="{{ old('usuario') ?: old('email') }}" required placeholder="Ingrese email o usuario..." autocomplete="login" autofocus="">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+  <div class="row justify-content-center" style="width: 70% !important;">
+    <div class="col-md-6">
+      <div class="card card-black">
+        <div class="card-body">
+        <h2 align="center" class="mb-4">INICIAR SESIÓN</h2>
+        <form action="{{ route('login') }}" method="post">
+          @csrf
+          <div class="mt-4">
+            
+            <input id="login" type="text" class="border border-warning form-control mb-4 input @if ($errors->has('usuario') || $errors->has('email')) is-invalid @endif" name="login" value="Usuario" required placeholder="Ingrese email o usuario..." autocomplete="login" autofocus="">
+            @if ($errors->has('usuario') || $errors->has('email'))
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('usuario') ?: $errors->first('email') }}</strong>
+              </span>
+            @endif
+            <input id="password" value="Contraseña" type="password" class="border border-warning form-control mb-4 input @error('password') is-invalid @enderror" name="password" required placeholder="*******************">
+            <div class="row">
+              <div class="col-6">
+                <div class="icheck-primary">
+                  <input type="checkbox" id="remember">
+                  <label for="remember">
+                    <span style="color: orange;">Recuérdame</span>
+                  </label>
+                </div>
+              </div>
+              <!-- /.col -->
+              <div class="col-6">
+                <button type="submit" class="btn btn-warning btn-block text-white" style="background-color: orange; border-color: orange;"><strong>Iniciar sesión</strong></button>
+              </div>
+              <!-- /.col -->
             </div>
           </div>
-          @if ($errors->has('usuario') || $errors->has('email'))
-            <span class="invalid-feedback" role="alert">
-              <strong>{{ $errors->first('usuario') ?: $errors->first('email') }}</strong>
-            </span>
-          @endif
+        </form>
+          
         </div>
-        <div class="input-group mb-3">
-          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required placeholder="*******************">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-6">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Recuerdame
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-6">
-            <button type="submit" class="btn btn-primary btn-block">Iniciar sesión </button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <!-- /.social-auth-links -->
-      @if (Route::has('password.request'))
-      <p class="mb-1">
-        <a href="#">¿Olvidó su contraseña?</a>
-      </p>
-      @endif
-      <p class="mb-0">
-        <a href="{{ route('registerc') }}" class="text-center">Registro de nuevo clientes</a>
-      </p>
+      </div>
     </div>
-    <!-- /.login-card-body -->
+    <!-- <div class="col-md-6"></div> -->
   </div>
-</div>
 <!-- /.login-box -->
 @endsection
