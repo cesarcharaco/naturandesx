8dfa5798bc4cbe53c05b10d0be11db8a5d97ab4d
Jhon-Dante <imagine.breacker@gmail.com>
2020-09-24 16:32:22 -0400

    logo - perfil

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -39,7 +39,7 @@
         <div class="card card-primary card-outline">
           <div class="card-body box-profile">
             <div class="text-center">
-              <img class="profile-user-img img-fluid img-circle" src="{{ asset('img/naturandes.jpg') }}" alt="User profile picture">
+              <img class="profile-user-img img-fluid img-circle" src="{{ asset('img/naturandes.jpg') }}" alt="User profile picture" style="width: 70%;">
             </div>
             <h3 class="profile-username text-center" style="text-transform: uppercase;">{{$users->clientes->nombres}} {{$users->clientes->apellidos}}</h3>
             <p class="text-muted text-center">{{$users->tipo_usuario}}</p>
@@ -222,7 +222,7 @@
         <div class="card card-primary card-outline">
           <div class="card-body box-profile">
             <div class="text-center">
-              <img class="profile-user-img img-fluid img-circle" src="{{ asset('img/naturandes.jpg') }}" alt="User profile picture">
+              <img class="profile-user-img img-fluid img-circle" src="{{ asset('img/naturandes.jpg') }}" alt="User profile picture" style="width: 70%;">
             </div>
             <h3 class="profile-username text-center" style="text-transform: uppercase;">{{$users->empleado->nombres}} {{$users->empleado->apellidos}}</h3>
             <p class="text-muted text-center">{{$users->tipo_usuario}}</p>
@@ -417,7 +417,7 @@
         <div class="card card-primary card-outline">
           <div class="card-body box-profile">
             <div class="text-center">
-              <img class="profile-user-img img-fluid img-circle" src="{{ asset('img/naturandes.jpg') }}" alt="User profile picture">
+              <img class="profile-user-img img-fluid img-circle" src="{{ asset('img/naturandes.jpg') }}" alt="User profile picture" style="width: 70%;">
             </div>
             <h3 class="profile-username text-center" style="text-transform: uppercase;">{{$users->empleado->nombres}} {{$users->empleado->apellidos}}</h3>
             <p class="text-muted text-center">{{$users->tipo_usuario}}</p>
