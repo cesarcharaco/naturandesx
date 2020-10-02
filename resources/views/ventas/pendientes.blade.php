683982a02274a815b790fd614e38607d192b9aa1
Jhon-Dante <imagine.breacker@gmail.com>
2020-10-02 17:52:52 -0400

    Arreglo vista - pendientes

--- resources/views/ventas/pendientes.blade.php
+++ resources/views/ventas/pendientes.blade.php
@@ -2,58 +2,95 @@
 @section('css')
 @endsection
 
+
+
+
+@section('content-header')
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Ventas por Pagar</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Ventas por Pagar</a></li>
+          <li class="breadcrumb-item active">Tablero</li>
+        </ol>
+      </div>
+    </div>
+  </div><!-- /.container-fluid -->
+</section>
+@endsection
+
+
 @section('content')
-<div class="content">             
-  <div class="breadcrumb-wrapper">
-    <h1>Ventas Pendientes por Pagar</h1>           
-    <nav aria-label="breadcrumb">
-      <ol class="breadcrumb p-0">
-        <li class="breadcrumb-item">
-          <a href="index.html"><span class="mdi mdi-home"></span></a>
-        </li>
-        <li class="breadcrumb-item">Ventas</li>
-        <li class="breadcrumb-item" aria-current="page">Pendientes por Pagar</li>
-      </ol>
-    </nav>
-  </div>
-
-  <div class="card bg-white shadow" style="border-radius: 30px !important;">
-    <div class="card-body">
-        
-      <div class="row">
-        <div class="col-md-8" style="position: relative !important;">
-          <table class="table dataTable data-table-basic table-curved table-striped tabla-estilo" style="width: 100%;">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>RUT</th>
-                
-              </tr>
-            </thead>
-            <tbody>
-              @foreach($repartidores as $key)
-              <tr>
-                <td>{{$num++}}</td>
-                <td>{{$key->nombres}}</td>
-                <td>{{$key->apellidos}}</td>
-                <td>{{$key->rut}}</td>
-                
-              </tr>
-              <tr>
-
-                <ul>
-                  @foreach($key->ventas as $key2)
-                  <li></li>
+<div class="container-fluid">
+  <div class="row mt-2 mb-2">
+    <div class="col-md-12">             
+      <div class="card bg-white shadow" style="border-radius: 30px !important;">
+        <div class="card-body">
+            
+          <div class="row">
+            <div class="col-md-12" style="position: relative !important;">
+              <table class="table dataTable data-table-basic table-curved table-striped tabla-estilo" style="width: 100%;">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>RUT</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($repartidores as $key)
+                  @php
+                    $cuenta=count($key->ventas);
+                    $cuenta2=1;
+                    $monto=0;
+                  @endphp
+                  <tr class="bg-info" class="text-white" id="empleado{{$key->id}}">
+                    <td><button id="boton{{$key->id}}" class="btn btn-success border" onclick="MostrarInfo(1,'{{$key->id}}')" style="border-radius: 30px;"><strong>+</strong></button></td>
+                    <td>{{$key->nombres}}</td>
+                    <td>{{$key->apellidos}}</td>
+                    <td>{{$key->rut}}</td>
+                  </tr>
+                    @foreach($key->ventas as $key2)
+                      <tr style="background-color: white; display: none;" class="fila{{$key->id}}">
+                        <td>{{$key2->cliente->nombres}} {{$key2->cliente->apellidos}}<br>{{$key2->cliente->rut}}</td>
+                        <td>{{$key2->created_at}}</td>
+                        <td>{{$key2->promociones->promocion}}<br>
+                          Cant. <strong>{{$key2->cantidad}}</strong><br>
+                          Monto Total: <strong>{{$key2->monto_total}}.00$</strong>
+                        </td>
+                          @foreach($key->empleados_has_ventas as $key3)
+                            @if($key2->id == $key3->id_venta)
+                              @if($key3->status == 'No Cancelado')
+                                <td><strong style="color: red;">{{$key3->status}}</strong></td>
+                                @php $monto = $monto+$key2->monto_total @endphp
+                              @else
+                                <td><strong style="color: green;">{{$key3->status}}</strong></td>
+                              @endif
+                            @endif
+                          @endforeach
+                      </tr>
+                      @if($cuenta == $cuenta2)
+                        <tr style="background-color: white; display: none;" class="fila{{$key->id}}">
+                          <td colspan="3" align="right"><h3>Total a pagar:</h3></td>
+                          <td><h3>{{$monto}}.00$</h3></td>
+                        </tr>
+                        @php $id=0 @endphp
+                      @endif
+                      @php $cuenta2++ @endphp
+                    @endforeach
                   @endforeach
-                </ul>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-        </div>
-        
+                </tbody>
+              </table>
+            </div>
+            
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -62,36 +99,26 @@
 @endsection
 
   <script type="text/javascript">
-    function RegistrarUser() {
-      $('.VistaLateralUsers').fadeOut('slow',
-        function() { 
-          $(this).hide();
-          $('#RegistrarUsers').fadeIn(300);
-      });
-    }
-
-    function verUser(id) {
-      $('.VistaLateralUsers').fadeOut('slow',
-        function() { 
-          $(this).hide();
-          $('#VerUsers').fadeIn(300);
-      });
-    }
-
-    function editarUser(argument) {
-      $('.VistaLateralUsers').fadeOut('slow',
-        function() { 
-          $(this).hide();
-          $('#EditarUsers').fadeIn(300);
-      });
-    }
-
-    function eliminarUser(id){
-      $('.VistaLateralUsers').fadeOut('slow',
-        function() { 
-          $(this).hide();
-          $('#EliminarUsers').fadeIn(300);
-      });
+    function MostrarInfo(opcion,id){
+      if (opcion == 1) {
+        // $('#empleado'+id).removeClass('bg-info').addClass('bg-success');
+        $('#boton'+id)
+          .removeClass('btn-success')
+          .addClass('btn-danger')
+          .html('<strong>-</strong>')
+          .removeAttr('onclick',false)
+          .attr('onclick','MostrarInfo(2,'+id+')');
+        $('.fila'+id).fadeIn(300);
+      }else{
+        // $('#empleado'+id).removeClass('bg-success').addClass('bg-info');
+        $('#boton'+id)
+          .removeClass('btn-danger')
+          .addClass('btn-success')
+          .html('<strong>+</strong>')
+          .removeAttr('onclick',false)
+          .attr('onclick','MostrarInfo(1,'+id+')');
+        $('.fila'+id).fadeOut('fast');
+      }
     }
   </script>
 @section('scripts')
