d0b8e9785353143236ad086325c83aab9fecc914
Jhon-Dante <imagine.breacker@gmail.com>
2020-10-03 12:37:56 -0400

    cambios

--- resources/views/ventas/pendientes.blade.php
+++ resources/views/ventas/pendientes.blade.php
@@ -30,65 +30,87 @@
     <div class="col-md-12">             
       <div class="card bg-white shadow" style="border-radius: 30px !important;">
         <div class="card-body">
-            
-          <div class="row">
-            <div class="col-md-12" style="position: relative !important;">
-                <table class="table table-bordered table-hover table-striped dataTable dtr-inline collapsed" style="width: 100% !important;">
-                  <thead class="text-capitalize bg-primary">
-                      <tr>
-                        <th>#</th>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>RUT</th>
-                        
-                      </tr>
-                  </thead>
-                  <tbody>
-                    @foreach($repartidores as $key)
-                      @if($key->usuario->tipo_usuario != 'Admin')
-                        @php
-                          $cuenta=count($key->ventas);
-                          $cuenta2=1;
-                          $monto=0;
-                        @endphp
-                        <tr class="bg-info" class="text-white" id="empleado{{$key->id}}">
-                          <td><button id="boton{{$key->id}}" class="btn btn-success border" onclick="MostrarInfo(1,'{{$key->id}}')" style="border-radius: 30px;"><strong>+</strong></button></td>
-                          <td>{{$key->nombres}}</td>
-                          <td>{{$key->apellidos}}</td>
-                          <td>{{$key->rut}}</td>
-                        </tr>
-                        @foreach($key->ventas as $key2)
-                          <tr style="background-color: white; display: none;" class="fila{{$key->id}}">
-                            <td>{{$key2->cliente->nombres}} {{$key2->cliente->apellidos}}<br>{{$key2->cliente->rut}}</td>
-                            <td>{{$key2->created_at}}</td>
-                            <td><strong>{{$key2->promociones->promocion}}</strong><br>
-                              Cant: <strong>{{$key2->cantidad}}</strong><br>
-                              Monto Total: <strong>{{$key2->monto_total}}.00$</strong>
-                            </td>
-                              @foreach($key->empleados_has_ventas as $key3)
-                                @if($key2->id == $key3->id_venta)
-                                  @if($key3->status == 'No Cancelado')
-                                    <td><strong style="color: red;">No Pagado</strong></td>
-                                    @php $monto = $monto+$key2->monto_total @endphp
-                                  @else
-                                    <td><strong style="color: green;">Pagado</strong></td>
-                                  @endif
-                                @endif
-                              @endforeach
+          <center>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label>Desde</label>
+                  <input id="inputDesde" max="<?php echo date('Y-m-d');?>" type="date" class="form-control shadow" name="desde" style="border-radius: 30px;" onchange="busqueda();">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label>Hasta</label>
+                  <input id="inputHasta" max="<?php echo date('Y-m-d');?>" type="date" class="form-control shadow" name="hasta" style="border-radius: 30px;" onchange="busqueda();">
+                </div>
+              </div>
+            </div>
+          </center>
+          <div class="shadow border border-default" style="border-radius: 20px;">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-12" style="position: relative !important;">
+                    <table id="example1" class="table table-bordered table-hover table-striped dataTable dtr-inline collapsed" style="width: 100% !important;">
+                      <thead class="text-capitalize bg-primary">
+                          <tr>
+                            <th>Nombre</th>
+                            <th>RUT</th>
+                            <th>Total</th>
                           </tr>
-                          @if($cuenta == $cuenta2)
-                            <tr style="background-color: white; display: none;" class="fila{{$key->id}}">
-                              <td colspan="2" align="right"><h3> Cantidad de promociones: <strong>{{ total_ventas_no_pagadas($key->id) }}</strong></h3></td> 
-                              <td colspan="2" align="right"><h3>Total a pagar: <strong>{{$monto}}.00$</strong></h3></td>
+                      </thead>
+                      <tbody>
+                        @foreach($repartidores as $key)
+                          @if($key->usuario->tipo_usuario != 'Admin')
+                            @php
+                              $cuenta=count($key->ventas);
+                              $cuenta2=1;
+                              $monto=0;
+                            @endphp
+                            <tr class="bg-info" class="text-white" id="empleado{{$key->id}}">
+                              <td>
+                                <button id="boton{{$key->id}}" class="btn btn-success border" onclick="MostrarInfo(1,'{{$key->id}}')" style="border-radius: 30px;"><strong>+</strong>
+                                </button>
+                                {{$key->nombres}} {{$key->apellidos}}</td>
+                              <td>{{$key->rut}}</td>
+                              <td>
+                                @if(total_ventas_no_pagadas($key->id) > 0)
+                                  <button class="btn btn-warning text-white" style="border-radius: 30px;"><strong>Pagar {{ total_ventas_no_pagadas($key->id) }}</strong></button></td>
+                                @endif
                             </tr>
-                            @php $id=0 @endphp
+                            @foreach($key->ventas as $key2)
+                              <tr style="background-color: white; display: none !important;" class="fila{{$key->id}}">
+                                <td>{{$key2->cliente->nombres}} {{$key2->cliente->apellidos}}<br>{{$key2->cliente->rut}}</td>
+                                <td>{{$key2->created_at}}</td>
+                                <td><strong>{{$key2->promociones->promocion}}</strong><br>
+                                  Cant: <strong>{{$key2->cantidad}}</strong><br>
+                                  <strong>{{$key2->monto_total}}.00$</strong>
+                                </td>
+                                  {{--@foreach($key->empleados_has_ventas as $key3)
+                                    @if($key2->id == $key3->id_venta)
+                                      @if($key3->status == 'No Cancelado')
+                                        <td><strong style="color: red;">No Pagado</strong></td>
+                                        @php $monto = $monto+$key2->monto_total @endphp
+                                      @else
+                                        <td><strong style="color: green;">Pagado</strong></td>
+                                      @endif
+                                    @endif
+                                  @endforeach--}}
+                              </tr>
+                              {{--@if($cuenta == $cuenta2)
+                                <tr style="background-color: white; display: none;" class="fila{{$key->id}}">
+                                  <td colspan="2" align="right"><h3> Cantidad de promociones: <strong>{{ total_ventas_no_pagadas($key->id) }}</strong></h3></td> 
+                                  <td colspan="2" align="right"><h3>Total a pagar: <strong>{{$monto}}.00$</strong></h3></td>
+                                </tr>
+                                @php $id=0 @endphp
+                              @endif--}}
+                              @php $cuenta2++ @endphp
+                            @endforeach
                           @endif
-                          @php $cuenta2++ @endphp
                         @endforeach
-                      @endif
-                    @endforeach
-                  </tbody>
-                </table>
+                      </tbody>
+                    </table>
+                </div>
+              </div>
             </div>
           </div>
         </div>
