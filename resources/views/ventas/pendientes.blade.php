78f0e158d089075c9e483d08c02e9ad64d8ec414
Jhon-Dante <imagine.breacker@gmail.com>
2020-10-05 12:36:53 -0400

    Vista pendientes

--- resources/views/ventas/pendientes.blade.php
+++ resources/views/ventas/pendientes.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 @section('css')
+  <title>Pendientes</title>
+  <!-- Include Choices CSS -->
+  <link rel="stylesheet" href="{{ asset('plugins/choices.js/choices.min.css') }}" />
 @endsection
-
-
-
 
 @section('content-header')
 <section class="content-header">
@@ -31,25 +31,66 @@
       <div class="card bg-white shadow" style="border-radius: 30px !important;">
         <div class="card-body">
           <center>
-            <div class="row">
-              <div class="col-md-6">
+            <div class="row mb-4">
+              <div class="col-md-3">
+                <div class="form-group">
+                  <label>Repartidor <b style="color: red;">*</b></label>
+                  <select class="form-control select2 choices form-select multiple-remove shadow" name="id_repartidor[]" required="required" style="border-radius: 30px;">
+                    <option value="">Seleccione Repartidor</option>
+                    @foreach($repartidores as $key)
+                      @if($key->usuario->tipo_usuario=="Empleado")
+                        <option value="{{$key->id}}" style="color: black !important;">{{$key->nombres}} {{$key->apellidos}}.- {{$key->rut}}</option>
+                      @endif
+                    @endforeach()
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-3">
                 <div class="form-group">
                   <label>Desde</label>
-                  <input id="inputDesde" max="<?php echo date('Y-m-d');?>" type="date" class="form-control shadow" name="desde" style="border-radius: 30px;" onchange="busqueda();">
+                  <input id="inputDesde" max="<?php echo date('Y-m-d');?>" type="date" class="form-control shadow" name="desde" style="border-radius: 30px;">
                 </div>
               </div>
-              <div class="col-md-6">
+              <div class="col-md-3">
                 <div class="form-group">
                   <label>Hasta</label>
-                  <input id="inputHasta" max="<?php echo date('Y-m-d');?>" type="date" class="form-control shadow" name="hasta" style="border-radius: 30px;" onchange="busqueda();">
+                  <input id="inputHasta" max="<?php echo date('Y-m-d');?>" type="date" class="form-control shadow" name="hasta" style="border-radius: 30px;">
                 </div>
               </div>
+              <div class="col-md-3">
+                <div class="card-body border shadow" style="border-radius: 30px;">
+                  <label for="">status</label>
+                  <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                      <div class="custom-control custom-checkbox">
+                          <input name="cancelado" type="checkbox" class="custom-control-input" id="customCheck1" value="1">
+                          <label class="custom-control-label" for="customCheck1">Pagado</label>
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="custom-control custom-checkbox">
+                        <input name="no_cancelado" type="checkbox" class="custom-control-input" id="customCheck2" value="1">
+                        <label class="custom-control-label" for="customCheck2">No Pagado</label>
+                      </div>                  
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="mt-4 mb-4">
+              <button class="btn btn-success">Generar</button>
             </div>
           </center>
           <div class="shadow border border-default" style="border-radius: 20px;">
             <div class="card-body">
               <div class="row">
                 <div class="col-md-12" style="position: relative !important;">
+                  <div id="cargando" class="mt-2 mb-2" style="display: none;">
+                    <center>
+                      <i class="fas fa-2x fa-sync-alt fa-spin"></i>
+                    </center>
+                  </div>
+                  <div id="tablePendientes">
                     <table id="example1" class="table table-bordered table-hover table-striped dataTable dtr-inline collapsed" style="width: 100% !important;">
                       <thead class="text-capitalize bg-primary">
                           <tr>
@@ -58,57 +99,10 @@
                             <th>Total</th>
                           </tr>
                       </thead>
-                      <tbody>
-                        @foreach($repartidores as $key)
-                          @if($key->usuario->tipo_usuario != 'Admin')
-                            @php
-                              $cuenta=count($key->ventas);
-                              $cuenta2=1;
-                              $monto=0;
-                            @endphp
-                            <tr class="bg-info" class="text-white" id="empleado{{$key->id}}">
-                              <td>
-                                <button id="boton{{$key->id}}" class="btn btn-success border" onclick="MostrarInfo(1,'{{$key->id}}')" style="border-radius: 30px;"><strong>+</strong>
-                                </button>
-                                {{$key->nombres}} {{$key->apellidos}}</td>
-                              <td>{{$key->rut}}</td>
-                              <td>
-                                @if(total_ventas_no_pagadas($key->id) > 0)
-                                  <button class="btn btn-warning text-white" style="border-radius: 30px;"><strong>Pagar {{ total_ventas_no_pagadas($key->id) }}</strong></button></td>
-                                @endif
-                            </tr>
-                            @foreach($key->ventas as $key2)
-                              <tr style="background-color: white; display: none !important;" class="fila{{$key->id}}">
-                                <td>{{$key2->cliente->nombres}} {{$key2->cliente->apellidos}}<br>{{$key2->cliente->rut}}</td>
-                                <td>{{$key2->created_at}}</td>
-                                <td><strong>{{$key2->promociones->promocion}}</strong><br>
-                                  Cant: <strong>{{$key2->cantidad}}</strong><br>
-                                  <strong>{{$key2->monto_total}}.00$</strong>
-                                </td>
-                                  {{--@foreach($key->empleados_has_ventas as $key3)
-                                    @if($key2->id == $key3->id_venta)
-                                      @if($key3->status == 'No Cancelado')
-                                        <td><strong style="color: red;">No Pagado</strong></td>
-                                        @php $monto = $monto+$key2->monto_total @endphp
-                                      @else
-                                        <td><strong style="color: green;">Pagado</strong></td>
-                                      @endif
-                                    @endif
-                                  @endforeach--}}
-                              </tr>
-                              {{--@if($cuenta == $cuenta2)
-                                <tr style="background-color: white; display: none;" class="fila{{$key->id}}">
-                                  <td colspan="2" align="right"><h3> Cantidad de promociones: <strong>{{ total_ventas_no_pagadas($key->id) }}</strong></h3></td> 
-                                  <td colspan="2" align="right"><h3>Total a pagar: <strong>{{$monto}}.00$</strong></h3></td>
-                                </tr>
-                                @php $id=0 @endphp
-                              @endif--}}
-                              @php $cuenta2++ @endphp
-                            @endforeach
-                          @endif
-                        @endforeach
+                      <tbody id="bodyPendiente">
                       </tbody>
                     </table>
+                  </div>
                 </div>
               </div>
             </div>
@@ -121,6 +115,46 @@
 @endsection
 
   <script type="text/javascript">
+
+    function busqueda() {
+      var desde = $('#inputDesde').val();
+      var hasta = $('#inputHasta').val();
+
+      if (desde && hasta) {
+        $('#tablePendientes').empty();
+        $('#inputDesde').attr('disabled',true);
+        $('#inputHasta').attr('disabled',true);
+        $('#cargando').fadeIn(300);
+
+
+
+        $.get("historial/"+desde+"/"+hasta+"/"+1+"/buscar",function (data) {
+        })
+        .done(function(data) {
+          if (data.length>0) {
+
+
+            n1 = data.length-1;
+            for (var i = 0; i < data.length; i++) {
+              busqueda2(data[i].id_venta,data[i].fecha,data[i].nombres,data[i].apellidos,data[i].promocion,data[i].cantidad,data[i].monto_total,n1,i);
+              
+            }
+            // $('#example1_wrapper').fadeIn(300);
+          }else{
+            $('#cargando').hide();
+            $('#inputDesde').removeAttr('disabled',false).val(0);
+            $('#inputHasta').removeAttr('disabled',false).val(0);
+            $('#sin_resultados').fadeIn(300);
+            setTimeout(function() {
+              $('#sin_resultados').fadeOut('slow');
+            }, 500);
+          }
+        });
+      }
+
+    }
+
+
     function MostrarInfo(opcion,id){
       if (opcion == 1) {
         // $('#empleado'+id).removeClass('bg-info').addClass('bg-success');
